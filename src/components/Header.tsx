8683cc5e7917822d455be3822c88f13e228e55da
Wesley Till <wesley.till@me.com>
2024-02-11 22:18:34 -0700

    set background onMouseEnter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Nav from 'react-bootstrap/Nav';
-import { MouseEvent } from 'react';
+import { MouseEvent, SetStateAction } from 'react';
 import { useState, useEffect } from 'react';
+import Fade from 'react-bootstrap/Fade';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import { CardBody } from 'react-bootstrap';
 
 const MyHeader: React.FC = (initialData) => {
   // const [dynamicData, setDynamicData] = useState(initialData);
@@ -17,12 +21,54 @@ const MyHeader: React.FC = (initialData) => {
   //   return;
   // }, []);
 
+  const [open, setOpen] = useState(false);
+
+  const [color, setColor] = useState("")
+  const click = (color: string) => {
+    setColor(color)
+  }
+
+  const [uncolor, unsetColor] = useState("")
+  const unclick = (color: string) => {
+    setColor(color)
+  }
+  /* This is where we actually
+     change background color */
+  useEffect(() => {
+    console.log("did something")
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundPosition = "50% 20%"
+    document.body.style.backgroundImage = color
+  }, [color])
+
+  useEffect(() => {
+    console.log("did something")
+    document.body.style.backgroundRepeat = "no-repeat"
+    document.body.style.backgroundSize = "cover"
+    document.body.style.backgroundPosition = "50% 20%"
+    document.body.style.backgroundImage = color
+  }, [uncolor])
+
+  function mouseEnterNav(navTab: string) {
+    //document.getElementById("tabPreview")!.src = "yeah boi"
+    click("url(apollo.jpeg)")
+    setOpen(true)
+  }
+  function mouseExitNav(navTab: string) {
+    unclick("")
+    setOpen(false)
+    //document.getElementById("tabPreview")!.innerHTML = "landing page ig"
+  }
+
   return (
     <>
       <Nav className="justify-content-center" fill variant="tabs">
         <Nav.Item
-          onMouseEnter={e => document.getElementById("landingPageText")!.innerHTML = "yeah boi"}
-          onMouseLeave={e => document.getElementById("landingPageText")!.innerHTML = "landing page ig"}>
+          onMouseEnter={e => mouseEnterNav("home")}
+          onMouseLeave={e => mouseExitNav("home")}
+          aria-controls="tabPreview"
+          aria-expanded={false}>
           <Nav.Link id="homeNav" href="/home">Active</Nav.Link>
         </Nav.Item>
         <Nav.Item>
@@ -34,10 +80,18 @@ const MyHeader: React.FC = (initialData) => {
           </Nav.Link>
         </Nav.Item>
       </Nav>
-      <Container>
-        <h1 id="landingPageText" >landing page?</h1>
-        <hr />
-      </Container>
+      <Button onClick={
+        () => click("url(apollo.jpeg)")
+      }>
+        hi
+      </Button>
+      <Fade in={open}>
+        <Container id="tabPreview" fluid>
+          <Card>
+            {/* <Card.Img src="apollo.jpeg" className="h-100" />; */}
+          </Card>
+        </Container>
+      </Fade>
     </>
   );
 }
