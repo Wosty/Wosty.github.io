790d1c0ce5c89ea53098cf5681aad76e46ea51e1
Wesley Till <wesley.till@me.com>
2024-02-25 15:13:34 -0700

    Updated content and polished formatting

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,13 +1,11 @@
-import Header from "@/components/Header";
-
 export default function PageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <>
-      <Header />
       <div>{children}</div>
     </>
   );
