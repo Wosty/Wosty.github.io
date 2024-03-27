a57aad6004378e4a5c2313e72debdf4cc0e87fd2
Wesley Till <wesley.till@me.com>
2024-03-26 20:02:51 -0600

    Setting up minimum viable product

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -19,10 +19,9 @@ const Header = () => {
           <NavbarCollapse id="responsive-navbar-nav">
             <Nav className="flex-grow-1 justify-content-evenly">
               <NavLink href="/resume">Professional Experience</NavLink>
-              <NavLink href="/about">About Me</NavLink>
-              <NavLink href="/food">Food Blog</NavLink>
-              <NavLink href="/apollo">Apollo's Gallery</NavLink>
-              <NavLink href="/contact">Contact</NavLink>
+              <NavLink href="/about" disabled>About Me</NavLink>
+              <NavLink href="/food" disabled>Food Blog</NavLink>
+              <NavLink href="/apollo" disabled>Apollo's Gallery</NavLink>
             </Nav>
           </NavbarCollapse>
         </Container>
