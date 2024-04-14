7bf6119687b1762b05eef36c5be28313890d3ea1
Wosty <36647710+Wosty@users.noreply.github.com>
2024-04-14 15:37:42 -0600

    Footer (#6)
    * Adding and implementing the footer component
    
    * Personalizing the README

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import { Container, Nav, NavLink, Navbar } from 'react-bootstrap';
+
+const Footer = () => {
+
+  return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Nav fill>
+            <NavLink href="https://github.com/Wosty/Wosty.github.io" className='px-4'>GitHub for this Website</NavLink>
+            <NavLink href="mailto:wesley.till99@gmail.com" className='px-4'>Email Me</NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Footer;
